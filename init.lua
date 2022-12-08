d9a2af0f997c75ad34c1f109e9134715ab955c18
Yusuf Bham <ybham6@gmail.com>
2022-12-08 01:55:03 -0500

    idk

--- init.lua
+++ init.lua
@@ -27,6 +27,11 @@ require('packer').startup(function(use)
             }
         end
     }
+    use { "ray-x/lsp_signature.nvim",
+        config = function()
+            require('lsp_signature').setup {}
+        end
+    }
     -- Show contexts
     use { 'nvim-treesitter/nvim-treesitter-context' }
 
@@ -95,6 +100,8 @@ require('packer').startup(function(use)
     use { "whonore/Coqtail",
         setup = function()
             vim.g.coqtail_nomap = 1
+            vim.api.nvim_set_hl(0, "CoqtailChecked", { bg = "#353b45" })
+            vim.api.nvim_set_hl(0, "CoqtailSent", { bg = "#333333" })
         end,
         config = function()
             vim.api.nvim_set_hl(0, "CoqtailChecked", { bg = "#353b45" })
@@ -139,6 +146,16 @@ require('packer').startup(function(use)
                 },
             }
 
+            lspconfig.arduino_language_server.setup {}
+            lspconfig.pyright.setup {}
+            lspconfig.clangd.setup {
+                cmd = {
+                    "clangd",
+                    '--query-driver=/home/home/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-g++',
+                },
+
+            }
+
             vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { desc = "Execute code action." })
             vim.keymap.set("n", "<space>ce", vim.lsp.buf.hover, { desc = "LSP hover action." })
         end,
@@ -147,9 +164,31 @@ require('packer').startup(function(use)
     -- LaTeX
     use { "lervag/vimtex",
         config = function()
-            vim.g.tex_flavor = 'latex'
-            vim.g.vimtex_view_method = 'zathura'
+            vim.g.tex_flavor = "latex"
+            vim.g.vimtex_view_method = "zathura"
             vim.g.vimtex_quickfix_mode = 0
+            -- vim.g.tex_conceal = "abdmg"
+            -- vim.g.tex_superscripts = "[a-zA-Z]"
+            -- vim.g.tex_subscripts="[a-zA-Z]"
+            --
+            vim.cmd [[
+                let g:vimtex_syntax_conceal = {
+                      \ 'accents': 1,
+                      \ 'ligatures': 1,
+                      \ 'cites': 1,
+                      \ 'fancy': 1,
+                      \ 'greek': 1,
+                      \ 'math_bounds': 1,
+                      \ 'math_delimiters': 1,
+                      \ 'math_fracs': 1,
+                      \ 'math_super_sub': 0,
+                      \ 'math_symbols': 1,
+                      \ 'sections': 0,
+                      \ 'styles': 1,
+                      \}
+            ]]
+
+            vim.cmd("set conceallevel=2")
         end
     }
 
@@ -270,17 +309,20 @@ require('packer').startup(function(use)
     -- }
 
     use { "SirVer/ultisnips",
+        ft = { 'tex' },
         setup = function()
             vim.g.UltiSnipsSnipeptsDir = vim.fn.stdpath("config") .. "/UltiSnips"
-            -- let g:UltiSnipsSnippetsDir = "~/.vim/bundle/ultisnips/UltiSnips"
+            -- just don't be tab, that lags shit
+            vim.g.UltiSnipsExpandTrigger = '<f5>'
+            vim.g.UltiSnipsJumpForwardTrigger="<C-Tab>"
+            vim.g.UltiSnipsJumpBackwardTrigger="<C-Shift-Tab>"
         end
     }
 
     use { "hrsh7th/nvim-cmp",
         -- after = "friendly-snippets",
         -- after = "LuaSnip",
-        require = "quangnguyen30192/cmp-nvim-ultisnips",
-        after = "ultisnips",
+        --require = "quangnguyen30192/cmp-nvim-ultisnips",
         setup = function()
             vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
         end,
@@ -310,7 +352,11 @@ require('packer').startup(function(use)
         after = "cmp-buffer",
     }
 
-    use("quangnguyen30192/cmp-nvim-ultisnips")
+    use {
+        ft = { 'tex' },
+        "quangnguyen30192/cmp-nvim-ultisnips",
+        require = "SirVer/ultisnips"
+    }
 
     -- misc plugins
     use { "windwp/nvim-autopairs",
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -12,9 +12,9 @@ local function border(hl_name)
 end
 
 local cmp = require("cmp")
-local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
+-- local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
 
-require("cmp_nvim_ultisnips").setup{}
+-- require("cmp_nvim_ultisnips").setup{}
 
 cmp.setup {
     window = {
@@ -26,35 +26,62 @@ cmp.setup {
             border = border "CmpDocBorder",
         },
     },
-    snippet = {
-        expand = function(args)
-            -- require("luasnip").lsp_expand(args.body)
-            vim.fn["UltiSnips#Anon"](args.body)
-        end,
-    },
+    -- snippet = {
+    --     -- expand = function(args)
+    --     --     -- require("luasnip").lsp_expand(args.body)
+    --     --     vim.fn["UltiSnips#Anon"](args.body)
+    --     -- end,
+    -- },
     mapping = {
         ["<C-p>"] = cmp.mapping.select_prev_item(),
         ["<C-n>"] = cmp.mapping.select_next_item(),
+
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
         ["<C-Space>"] = cmp.mapping.complete(),
+
         ["<C-e>"] = cmp.mapping.close(),
+
         ["<CR>"] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = false,
         },
 
+        -- ["<Tab>"] = cmp.mapping(
+        --   function(fallback)
+        --     cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
+        --   end,
+        --   { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+        -- ),
+        -- ["<S-Tab>"] = cmp.mapping(
+        --   function(fallback)
+        --     cmp_ultisnips_mappings.jump_backwards(fallback)
+        --   end,
+        --   { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+        -- ),
+
         ["<Tab>"] = cmp.mapping(
-          function(fallback)
-            cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
-          end,
-          { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+            function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                -- elseif require("luasnip").expand_or_jumpable() then
+                --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+                else
+                    fallback()
+                end
+            end, { "i", "s", }
         ),
         ["<S-Tab>"] = cmp.mapping(
-          function(fallback)
-            cmp_ultisnips_mappings.jump_backwards(fallback)
-          end,
-          { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+            function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                -- elseif require("luasnip").jumpable(-1) then
+                --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+                else
+                    fallback()
+                end
+            end, { "i", "s", }
         ),
 
         -- ["<Tab>"] = cmp.mapping(function(fallback)
